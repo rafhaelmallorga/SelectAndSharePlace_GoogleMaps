b1323a6d1e35a3d5eb3a300791d69592b69af026
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2024-10-06 00:29:10 -0300

    new: project basic structure

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("####TESTE WEBPACK")})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiJtQkFBQUEsUUFBUUMsSUFBSSxvQiIsInNvdXJjZXMiOlsid2VicGFjazovL3NlbGVjdGFuZHNoYXJlcGxhY2VfZ29vZ2xlbWFwcy8uL3NyYy9hcHAudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCIjIyMjVEVTVEUgV0VCUEFDS1wiKSJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
